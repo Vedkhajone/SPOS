1210b9878ca0392d135cd89afb22a54fef36d92d
Ved Khajone <143710946+Vedkhajone@users.noreply.github.com>
2025-11-10 10:47:03 +0530

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,24 +1,34 @@
-#!/bin/sh -i
-clear
-echo "PROGRAM TO PERFORM ARITHMETIC OPERATION ON 2 INTEGER NUMBERS"
+
+#!/bin/sh
 
 ch=1
-while [ $ch -eq 1 ]; do
-echo -n "Enter First Integer number:    "
-read a
-echo -n "Enter Second integer number:   "
-read b
-echo -n "1=ADDITION 2=SUBTRACTION 3=MULTIPLICATION, 4=DIVISION Enter choice:  "
-read ch
+while [ $ch -eq 1 ]
+do
+    clear
+    echo "PROGRAM TO PERFORM ARITHMETIC OPERATION ON 2 INTEGER NUMBERS"
+    echo -n "Enter first integer number: "
+    read a
+    echo -n "Enter second integer number: "
+    read b
 
-case $ch in
-1) echo "RESULT OF  $a + $b = $(($a+$b))" ;;
-2) echo "RESULT OF  $a - $b = $(($a-$b))";;
-3) echo "RESULT OF  $a X $b = $(($a * $b))";;
-4) echo "RESULT OF  $a / $b = $(($a/$b)).$(($a%$b))";;
-*) echo "You have not entered proper choice"
-esac
-echo "would u like to continue YES=1/NO=0"
-read ch
+    echo "1 = ADDITION"
+    echo "2 = SUBTRACTION"
+    echo "3 = MULTIPLICATION"
+    echo "4 = DIVISION"
+    echo -n "Enter your choice: "
+    read c
+
+    case $c in
+        1) echo "RESULT of $a + $b = expr $a + $b";;
+        2) echo "RESULT of $a - $b = expr $a - $b";;
+        3) echo "RESULT of $a * $b = expr $a \* $b";;
+        4) echo "RESULT of $a / $b = expr $a / $b";;
+        *) echo "Invalid choice";;
+    esac
+
+    echo -n "Would you like to continue? (Yes=1 / No=0): "
+    read ch
+
 done
+
 exit 0
